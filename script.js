56aca27acf4974d99c3b647a6a57161fab376e3b
theintheinsoe1218 <theintheinsoe1218@gmail.com>
2022-01-02 21:11:53 +0630

    adding click event in js file

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const textareaTag=document.querySelector(".textarea");
 const buttonTag=document.querySelector(".btn");
+const textContainerTag=document.querySelector(".textContainer");
 buttonTag.addEventListener("click",()=>{
-  console.log("hey");
+  // console.log("hey");
+
+  const textareaText=textareaTag.value;
+  // console.log(textareaText);\
+  if(textareaText.length===0){
+    return;
+  }
+  const textTag=document.createElement("div");
+  textTag.classList.add("textTag");
+  textTag.append(textareaText);
+  textContainerTag.append(textTag);
 });
